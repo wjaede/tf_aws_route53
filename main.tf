0754f0f2495d3a9d692c7b57c962b7dabed4a8df
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:21:20 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 resource "aws_route53_zone" "domain" {
   name = "${var.domain}"
   vpc_id = "${var.vpc_id}"
